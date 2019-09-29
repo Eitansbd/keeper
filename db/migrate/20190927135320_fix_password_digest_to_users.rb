44a039606a9da910ef0dd35ac4bd6f5f721964f9
Eitan Bar-David <eitansbd@gmail.com>
2019-09-29 02:05:30 +0000

    fix migration issue

--- db/migrate/20190927135320_fix_password_digest_to_users.rb
+++ db/migrate/20190927135320_fix_password_digest_to_users.rb
@@ -1,6 +1,5 @@
 class FixPasswordDigestToUsers < ActiveRecord::Migration[5.0]
   def change
-    remove_column :users, :password__digests
     add_column :users, :password_digest, :string
   end
 end
