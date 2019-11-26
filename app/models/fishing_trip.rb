2f132dfa38c47004496dd0ebfcefdd7271a70507
Eitan Bar-David <eitansbd@gmail.com>
2019-11-26 13:45:38 +0000

    add validations and recent scope

--- app/models/fishing_trip.rb
+++ app/models/fishing_trip.rb
@@ -1,6 +1,17 @@
 class FishingTrip < ApplicationRecord
   belongs_to :user
+  has_many :fish_catches
   
-  validates :user_id, presence: true
-  validates :content, presence: true
+  validates_presence_of :title, :content, :weather, :body_of_water, :location,
+                        :date, :start_time, :end_time
+                        
+  validate :end_after_start
+
+  scope :recent, -> { eager_load(:user).order(created_at: :desc).limit(5) }
+  
+  def end_after_start
+    if start_time && end_time && start_time >= end_time
+      errors.add(:end_time, "must be after start time")
+    end
+  end
 end
