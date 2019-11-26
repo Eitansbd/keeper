823217ee09d807cead4677f69845ac8dd5842e82
Eitan Bar-David <eitansbd@gmail.com>
2019-11-26 13:51:56 +0000

    add new and create

--- app/controllers/fishing_trips_controller.rb
+++ app/controllers/fishing_trips_controller.rb
@@ -1,5 +1,5 @@
 class FishingTripsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]  
+  before_action :logged_in_user, only: [:new, :create, :destroy]  
   
   def new
     @fishing_trip = FishingTrip.new
@@ -7,12 +7,18 @@ class FishingTripsController < ApplicationController
   
   def create
     @fishing_trip = current_user.fishing_trips.build(fishing_trip_params)
+    
     if @fishing_trip.save 
       flash[:success] = "Fishing trip saved!"
       redirect_to root_url
     else
-      render 'static_page/home'
+      render 'new'
     end
+  end
+  
+  def show
+    @fishing_trip = FishingTrip.find(params[:id])
+    @fish_types = FishType.all
   end
   
   def destroy
@@ -21,6 +27,8 @@ class FishingTripsController < ApplicationController
   private
     
     def fishing_trip_params
-      params.require(:fishing_trip).permit(:content)
+      params.require(:fishing_trip).permit(:title, :content, :weather, 
+                                           :body_of_water, :location, :date,
+                                           :start_time, :end_time)
     end
 end
